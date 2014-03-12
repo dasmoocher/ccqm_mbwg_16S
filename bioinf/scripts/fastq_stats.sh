aac37873fa2bc1f774559e9df4de71651fac690a
Nate Olson <nolson@smai.nist.gov>
2014-03-12 16:13:36 -0400

    Modified fastq stats script and generated gather script

--- bioinf/scripts/fastq_stats.sh
+++ bioinf/scripts/fastq_stats.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 # fastq basic stats
-for fq in data/*fastq;
+for fq in *fastq;
 do
 	out=$(echo $fq | sed 's/.fastq/-fastq-stats.txt/')
-	perl bin/prinseq-lite.pl -stats_all -fastq $fq > $out
+	perl ../bin/prinseq-lite.pl -stats_all -fastq $fq > $out
 done
---
+++ stats/gather_fastq-stats.R
@@ -0,0 +1,25 @@
+# user specific requirements
+source("file_locations.R")
+
+# script for creating base count and coverage files for coverage figures and base ratio analysis
+library(stringr)
+library(reshape2)
+library(plyr)
+
+
+read_table <- function(filename){
+  stat_table <- read.table(filename, 
+                          sep="\t", header = FALSE, na.strings = NA)
+  ds <- filename
+  ds <-str_replace(ds, pattern="-fastq-stats.txt", replace="")
+  stat_table$dataset <- ds
+  return(stat_table)
+}
+
+setwd(bioinf_data_loc)
+stat_tables <- list.files()[grep("*fastq-stats.txt", c(list.files()), value = F)]
+stat_df <- ldply(stat_tables, read_table)
+colnames(stat_df) <- c("metric","variable","value", "dataset")
+stat_df$metric <- str_replace(stat_df$metric, "stats_","")
+setwd(script_loc)
+write.csv(stat_df, str_c(stat_data_loc, "merged_fastq_stats.csv", sep = ""), row.names = FALSE)
