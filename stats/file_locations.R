aa0b15f296f82cac36cda1a4f0e5adc4cf524e83
vagrant <vagrant@ubuntu1204-docker.(none)>
2014-05-28 05:00:59 -0700

    changed reference for file_location.R

--- stats/file_locations.R
+++ stats/file_locations.R
@@ -1,8 +1,10 @@
 ## Specifying file directories
 require(stringr)
-ccqm16S_loc <- "~/Desktop/ccqm_mbwg_16S/"
+setwd("../")
+ccqm16S_loc <- getwd()
 stat_data_loc <- str_c(ccqm16S_loc,"stats/data/", sep = "")
 bioinf_data_loc <- str_c(ccqm16S_loc,"bioinf/results/", sep = "")
 script_loc <- str_c(ccqm16S_loc,"stats/", sep = "")
 figure_loc <- str_c(ccqm16S_loc,"pub/", sep = "")
 sup_loc <- str_c(ccqm16S_loc,"pub/", sep = "")
+setwd(script_loc)
