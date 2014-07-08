ec7d41149e14eaecb7f83d89f100216760bc4a05
Nate Olson <nathandavidolson@gmail.com>
2014-07-08 07:27:15 -0400

    moved pipe.sh to root directory

---
+++ pipe.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#####
+# bash script which executes a series of bash scripts that perform the initial 
+# bioinformatic data analysis of the 2013 CCQM MBSG 16S rRNA interlaboratory 
+# sequencing study
+#
+# Created by: Nate Olson
+#
+# Updated June 2, 2014
+#
+#####
+
+cd bioinf
+bash scripts/sra_to_fastq.sh
+cd results
+bash ../scripts/ccqm_pipeline_comparison.sh \
+	../resources/metadata.csv \
+	../resources/Lmono_16S_consensus_N.fasta \
+	../resources/Ecoli_16S_consensus_N.fasta
+bash ../scripts/fastq_stats.sh
+bash ../scripts/pipeline_comparison_tables.sh
+bash ../scripts/base_ratio_primary_analysis.sh
+bash ../scripts/variant_combinations.sh
+cd ../../stats
+R CMD BATCH ccqm_stat_pipe.R
